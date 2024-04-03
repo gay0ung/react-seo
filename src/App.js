120adf2b88f97b2ec9668616bda615b0032b4770
gay0ung <58800923+gay0ung@users.noreply.github.com>
2024-04-03 20:58:33 +0900

    added meta tags

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import gaYoung from './images/KakaoTalk_20240401_211820418.jpg'
 import './App.css';
+import { Helmet } from 'react-helmet';
 
 function App() {
   return (
     <div className="App">
+      <Helmet>
+        <title>lee ga young</title>
+        <meta name="description" content="ga young app application" />
+
+        {/* open graph tag */}
+        <meta property="og:title" content="ga young app" />
+        <meta property="og:description" content="ga young app application" />
+        <meta property="og:image" content="./images/KakaoTalk_20240401_211820418.jpg0" />
+        <meta property="og:url" content="" />
+        <meta property="og:type" content="website" />
+
+        {/* twitter tag */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="My Website" />
+        <meta name="twitter:description" content="This is my website." />
+        <meta name="twitter:image" content="./images/KakaoTalk_20240401_211820418.jpg0" />
+      </Helmet>
+
       <div className='card'>
         <h1>안녕하세요</h1>
         <p>제 이름은 "이가영" 입니다.</p>
