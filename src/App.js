f7f18392cd5fde8eef95a7a61ee68a75345009a7
gay0ung <58800923+gay0ung@users.noreply.github.com>
2024-04-03 22:38:25 +0900

    이미지 파일 경로 수정

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import gaYoung from './images/KakaoTalk_20240401_211820418.jpg'
+import gaYoung from './images/ga-young.jpg'
 import './App.css';
 import { Helmet } from 'react-helmet';
 
@@ -12,7 +12,7 @@ function App() {
         {/* open graph tag */}
         <meta property="og:title" content="ga young app" />
         <meta property="og:description" content="ga young app application" />
-        <meta property="og:image" content="./images/KakaoTalk_20240401_211820418.jpg" />
+        <meta property="og:image" content="https://react-seo-ochre.vercel.app/images/ga-young.jpg" />
         <meta property="og:url" content="https://react-seo-ochre.vercel.app/" />
         <meta property="og:type" content="website" />
 
@@ -20,7 +20,7 @@ function App() {
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content="My Website" />
         <meta name="twitter:description" content="This is my website." />
-        <meta name="twitter:image" content="./images/KakaoTalk_20240401_211820418.jpg0" />
+        <meta name="twitter:image" content="https://react-seo-ochre.vercel.app/images/ga-young.jpg" />
       </Helmet>
 
       <div className='card'>
